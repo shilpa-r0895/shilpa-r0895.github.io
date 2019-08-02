8f776a759cbe0aa61ba4d9eb63608aa2688f46f5
Shilpa <shilpa@signzy.com>
2019-08-02 20:46:06 +0530

    Initial commit

---
+++ precache-manifest.76ed1b6f0a37036123294d6f57f968be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61fc25b1dd89d66ae1eecaaa4473563",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e99b2eebf54a59fc3db",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "6b379efa5e7bda0e3359",
+    "url": "/static/css/main.e6dfcf36.chunk.css"
+  },
+  {
+    "revision": "5e99b2eebf54a59fc3db",
+    "url": "/static/js/2.d68e257d.chunk.js"
+  },
+  {
+    "revision": "6b379efa5e7bda0e3359",
+    "url": "/static/js/main.d30b14cf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
